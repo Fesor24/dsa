c6eae6a9ec921765f5c2f611f2ead678f3e26ca0
Wale <onafesowale@gmail.com>
2025-05-05 12:33:05 +0100

    two-sums

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,28 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Array;
+
 Console.WriteLine("Arrays!!!");
 
-// when we declare an array like this, a1 is this case is a null reference
-// memory for keeping elements is not allocated yet so s1 does not point to any memory location
-int[] a1;
-
-// in this case, a chunk of memory will be allocated for keeping 10 integer values
-a1 = new int[10];
-
-// multi dimension array
-int[,] multiDimArray = new int[2,3]{{1,2,3}, {8,3,1}};
-int[,] multiDimArray2 = { { 2, 3, 9 }, { 0, 9, 1 } };
-
-for (int i = 0; i < multiDimArray.GetLength(0); i++)
+var result = TwoSums.RunOptimized();
+foreach (var item in result)
 {
-    for (int j = 0; j < multiDimArray.GetLength(1); j++)
+    string res = $"{item.Key} - Values = ";
+    foreach (int value in item.Value)
     {
-        Console.WriteLine($"{multiDimArray[i, j]}");
+        res += $"{value},";       
     }
+    Console.WriteLine($"{res}");
 }
-
-// jagges array
-int [][] jaggedArr = new int[2][];
-
-jaggedArr[0] = new int[3]; // first row will have 3 items
-jaggedArr[1] = new int[2]; // second row will have 2 items
---
+++ Array/TwoSums.cs
@@ -0,0 +1,54 @@
+﻿namespace Array;
+
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
+public class TwoSums
+{
+    private static readonly int[] testCase1 = [2,7,11,15];
+    private static readonly int target1 = 9;
+    
+    private static readonly int[] testCase2 = [3,2,4];
+    private static readonly int target2 = 6;
+    
+    private static readonly int[] testCase3 = [3,3];
+    private static readonly int target3 = 6;
+    
+    public static int[] BruteForce(int[] nums, int target)
+    {
+        return [];
+    }
+
+    public static Dictionary<string, int[]> RunOptimized()
+    {
+        int[] answer1 = Optimized(testCase1, target1);
+        int[] answer2 = Optimized(testCase2, target2);
+        int[] answer3 = Optimized(testCase3, target3);
+
+        return new Dictionary<string, int[]>()
+        {
+            { "Test case 1", answer1 },
+            { "Test case 2", answer2 },
+            { "Test case 3", answer3 },
+        };
+    }
+
+    private static int[] Optimized(int[] nums, int target)
+    {
+        Dictionary<int, int> maps = new();
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int remainder = target - nums[i];
+            
+            // if the remainder is in the dictionary, return the indices
+            if (maps.ContainsKey(remainder))
+                return new int[2] { i, maps[remainder] };
+            
+            // if current iteration is not in the dictionary, add it to the dictionary
+            if(!maps.ContainsKey(nums[i]))
+                maps.Add(nums[i], i);
+        }
+        
+        throw new Exception("No solution found");
+    }
+}
