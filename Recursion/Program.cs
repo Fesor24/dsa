6c95b30fd52af963398dfbf09beeb7e9a9c20b12
Wale <onafesowale@gmail.com>
2025-05-12 17:25:16 +0100

    recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+WriteLine("Recursion!!");
+
+int n = 4;
+SubtractAndPrint(n);
+
+ReadLine();
+
+void SubtractAndPrint(int x)
+{
+    if(x > 0)
+    {
+        WriteLine($"Value of n is {x}");
+        SubtractAndPrint(x - 1);
+    }
+}
